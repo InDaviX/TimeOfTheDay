780af1af87e78e06ff8f513929e380f29f005ca0
InDaviX <dszewczyk235@gmail.com>
2023-12-19 22:13:43 +0100

    Wersja ostateczna na stan 19.12.2023

--- components/TimeOfDay/TimeOfDay.js
+++ components/TimeOfDay/TimeOfDay.js
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import style from './style';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, Animated} from 'react-native';
 
 const TimeOfDay = () => {
   const [timeOfDay, setTimeOfDay] = useState('evening');
   const [clock, setClock] = useState('00:00');
+  const flyInAnimation = useRef(new Animated.Value(-200)).current;
 
   useEffect(() => {
     const currentHour = new Date().getHours();
@@ -18,13 +19,17 @@ const TimeOfDay = () => {
     } else {
       setTimeOfDay('night');
     }
-  }, []);
 
-  useEffect(() => {
     const h = new Date().getHours() + 1;
     const m = new Date().getMinutes();
-    setClock(`${h}:${m}`);
-  }, []);
+    setClock(`${h}:${m < 10 ? `0${m}` : m}`);
+
+    Animated.spring(flyInAnimation, {
+      toValue: 0,
+      speed: 2,
+      useNativeDriver: true,
+    }).start();
+  }, [flyInAnimation]);
 
   return (
     <SafeAreaView style={{height: '100%'}}>
@@ -33,14 +38,37 @@ const TimeOfDay = () => {
         colors={['#762FD3', '#E2DBB8']}
         start={{x: -0.3, y: 0}}
         end={{x: 0, y: 1}}>
-        <Text style={style.text}>Good {timeOfDay}!</Text>
+        <Animated.View
+          style={[
+            {
+              transform: [
+                {
+                  translateX: flyInAnimation,
+                },
+              ],
+            },
+          ]}>
+          <Text style={style.text}>Good {timeOfDay}!</Text>
+        </Animated.View>
       </LinearGradient>
+
       <LinearGradient
         style={[style.view2, style.gradient]}
         colors={['#762FD3', '#E2DBB8']}
         start={{x: 1, y: 0}}
         end={{x: 0, y: 1}}>
-        <Text style={style.text2}>It's {clock}!</Text>
+        <Animated.View
+          style={[
+            {
+              transform: [
+                {
+                  translateX: flyInAnimation,
+                },
+              ],
+            },
+          ]}>
+          <Text style={style.text2}>It's {clock}!</Text>
+        </Animated.View>
       </LinearGradient>
     </SafeAreaView>
   );
